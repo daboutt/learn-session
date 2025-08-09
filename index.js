adec4f98f795350668ecce4b82147c4356dbe3be
Gia Bao <giabao@Gias-MacBook-Air.local>
2025-08-09 12:15:21 +0700

    session

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const session = require('express-session');
+
+//Create in-memory session
+const store = new session.MemoryStore();
+
+//Create session
+app.use(
+  session({
+    secret: '232jksf@',
+    resave: false,
+    saveUninitialized: false,
+    store,
+  })
+);
+
+// NOTE: Note: Storing in-memory sessions is something that should be done only during development,
+// NOT during production due to security risks
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`server is listen on por ${PORT}`);
+});
